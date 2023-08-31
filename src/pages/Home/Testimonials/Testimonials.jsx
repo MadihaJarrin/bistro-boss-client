e8fc9c2d329fad0975a81493db0fd11c4b4e9553
MadihaJarrin <madihajarrin1@gmail.com>
2023-09-01 04:32:28 +0600

    added

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -10,10 +10,19 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
 
 
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
 
+    useEffect(() => {
+        fetch('reviews')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }
+
+        , [])
 
 
     return (
@@ -26,15 +35,15 @@ const Testimonials = () => {
             </SectionTitle>
 
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+
+                {
+                    reviews.map(review =>
+                        <SwiperSlide
+                            key={review._id}
+
+                        ></SwiperSlide>
+                    )
+                }
             </Swiper>
 
         </section>
