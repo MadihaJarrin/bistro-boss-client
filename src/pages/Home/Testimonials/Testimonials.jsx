c7e7c25fd99201115882d28a9ac2c137b383e343
MadihaJarrin <madihajarrin1@gmail.com>
2023-09-02 06:23:53 +0600

    added

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -12,18 +12,18 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import { useEffect, useState } from "react";
 
+import '@smastrom/react-rating/style.css'
+import { Rating } from "@smastrom/react-rating";
+
 
 const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch('reviews')
+        fetch('reviews.json')
             .then(res => res.json())
             .then(data => setReviews(data))
-    }
-
-        , [])
-
+    }, [])
 
     return (
         <section className="my-20">
@@ -40,8 +40,23 @@ const Testimonials = () => {
                     reviews.map(review =>
                         <SwiperSlide
                             key={review._id}
+                        >
 
-                        ></SwiperSlide>
+                            <div className="flex flex-col items-center mx-24 my-16">
+                                <h2>REVIEW COUNT : {reviews.length}</h2>
+
+                                <Rating
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    readOnly
+                                >
+
+                                </Rating>
+
+                                <p className="py-8">{review.details}</p>
+                                <h3 className="text-2xl text-orange-400">{review.name}</h3>
+                            </div>
+                        </SwiperSlide>
                     )
                 }
             </Swiper>
